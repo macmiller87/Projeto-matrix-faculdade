b6f395238296fec93843cb8edf302011b59d8637
macmiller87 <macamagolf@gmail.com>
2020-10-09 15:01:48 -0300

    Ajuste

--- docs/javascript/matrix.js
+++
@@ -1,9 +0,0 @@
-function mudafoto(foto) {
-    document.getElementById("icone").src = foto;
-}
-
-function calc_total() {
-    var qtd = parseInt(document.getElementById("cqtd").value);
-    var tot = qtd * 1500;
-    document.getElementById("ctot").value = tot;
-}
